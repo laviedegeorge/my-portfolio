38152a352f7c19712dbd6979e9caf561a2a7cb1b
laviedegeorge <laviedegeorge1292@gmail.com>
2020-12-21 16:32:43 +0100

    create sample structure of the portfolio API

---
+++ api-data-structure/sample-structure.js
@@ -0,0 +1,74 @@
+/* 
+  - Please this is only a sample of information i would be using.
+  - The data structure is subject to change because you know better than me.
+  - Please you can create your struture in a different file if possible.
+  - Also if you don't mind i could show it to my boss to for his comments.
+  _ If any of the data is not necessary please let me know.
+*/
+
+const data = {
+  personalInformation: {
+    name: "Kelechi George Apugo",
+    email: "laviedegeorge1292@gmail.com",
+    phoneNumber: "08062242901",
+    imgurl: ["img1", "img2", "img3"], // an array just incase there is more than one image to be used.
+    aboutMe: "10x Frontend web Engineer",
+    keyWords: ["Badass", "Team Player", "PubG/COD lover"],
+    socialMediaHandles: {
+      linkedin: {
+        name: "",
+        username: "",
+        url: "",
+        // more if needed.
+      },
+      github: {
+        name: "",
+        username: "",
+        url: "",
+        // more if needed.
+      },
+      twitter: {
+        name: "",
+        username: "",
+        url: "",
+        // more if needed.
+      },
+    },
+    // more info if any
+  },
+  exp: [
+    {
+      name: "HTML",
+      level: "70",
+    },
+    {
+      name: "CSS",
+      level: "70",
+    },
+    {
+      name: "JavaScript",
+      level: "70",
+    },
+    // and so on ...
+  ],
+  porfolio: [
+    {
+      name: "Todo App",
+      imgUrl: ["img1", "img2", "img3"],
+      url: "",
+      description: "",
+    },
+    {
+      name: "Todo App",
+      imgUrl: ["img1", "img2", "img3"],
+      url: "",
+      description: "",
+    },
+    {
+      name: "Todo App",
+      imgUrl: ["img1", "img2", "img3"],
+      url: "",
+      description: "",
+    },
+  ],
+};
