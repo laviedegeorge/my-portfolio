088c7c616d0852be1091c75432493390a61ebc65
Kelechi <laviedegeorge1292@gmail.com>
2026-04-15 10:58:07 +0100

    scaffold repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,12 @@
 "use client";
 import { Footer } from "@/components/layouts/footer";
 import { Header } from "@/components/layouts/header";
-import { AboutUs } from "@/components/sections/landing-page/about-us";
-import { ClientsPartners } from "@/components/sections/landing-page/client-partners";
-import { Hero } from "@/components/sections/landing-page/hero";
-import { Services } from "@/components/sections/landing-page/services";
-import { Team } from "@/components/sections/landing-page/team";
-import { Values } from "@/components/sections/landing-page/values";
-import { VisionMission } from "@/components/sections/landing-page/vision-mission";
-import { WhyChooseUs } from "@/components/sections/landing-page/why-choose-us";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
-      <main className="min-h-screen">
-        <Hero />
-        <VisionMission />
-        <AboutUs />
-        <Services />
-        <WhyChooseUs />
-        <ClientsPartners />
-        <Values />
-        <Team />
-      </main>
+      <main className="min-h-screen"></main>
       <Footer />
     </div>
   );
