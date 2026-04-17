4974e8b38a9800999cbc31b1ef109eb53d7a1730
Kelechi <laviedegeorge1292@gmail.com>
2026-04-17 10:50:00 +0100

    implement impact us section in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/layouts/header";
 import About from "@/components/sections/landing-page/about-us";
 import Hero from "@/components/sections/landing-page/hero";
+import Impact from "@/components/sections/landing-page/impact";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main>
         <Hero />
         <About />
+        <Impact />
       </main>
     </>
   );
