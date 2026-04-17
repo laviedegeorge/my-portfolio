d6f62dee9747b756b012a25a4107d60bbd9f8d0e
Kelechi <laviedegeorge1292@gmail.com>
2026-04-17 20:01:47 +0100

    implement programs and NCD  section in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Header from "@/components/layouts/header";
 import About from "@/components/sections/landing-page/about-us";
 import Hero from "@/components/sections/landing-page/hero";
 import Impact from "@/components/sections/landing-page/impact";
+import NCD from "@/components/sections/landing-page/ncd";
+import Programs from "@/components/sections/landing-page/programs";
 
 export default function Home() {
   return (
@@ -11,6 +13,8 @@ export default function Home() {
         <Hero />
         <About />
         <Impact />
+        <Programs />
+        <NCD />
       </main>
     </>
   );
