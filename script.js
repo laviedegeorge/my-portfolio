5c9b42fb9983240ae0a77d5fc95a431963f43bdc
Kelechi <laviedegeorge1292@gmail.com>
2026-04-22 11:45:17 +0100

    clear old files

--- script.js
+++
@@ -1,9 +0,0 @@
-const dropDownBtn = document.getElementById('dropDownBtn');
-dropDownBtn.addEventListener('click', () => {
-    if (navCont.style.display === 'block') {
-        navCont.style.display = 'none';
-    } else {
-        navCont.style.display = 'block';
-    }
-    
-})
